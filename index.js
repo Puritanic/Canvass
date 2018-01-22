0e67d08a42c741f87296d170f6888bca1047639f
Puritanic <darktasevski@gmail.com>
2018-01-23 00:30:16 +0100

    Add Heroku postbuild and set up routes handling

--- index.js
+++ index.js
@@ -34,9 +34,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like main.js and main.css that are bundled inside CRA
+  app.use(express.static('client/build'));
+  // Express will serve index.html file if it doesn't recognize the route (404)
+  /* eslint-disable */
+  const path = require('path');
+  /* eslint-enable */
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
